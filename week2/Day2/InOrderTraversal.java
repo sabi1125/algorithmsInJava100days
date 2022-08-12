70842405f44a7145fcb51a3e5b167ae43508066d
sabi1125 <sabir_barahi4@hotmail.com>
2022-08-12 21:05:40 +0900

    day 9

---
+++ week2/Day2/InOrderTraversal.java
@@ -0,0 +1,25 @@
+package week2.Day2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+import javax.swing.tree.TreeNode;
+
+class InOrderTraversal {
+  public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            res.add(curr.val);
+            curr = curr.right;
+        }
+        return res;
+    }
+}
